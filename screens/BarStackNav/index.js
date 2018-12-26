2af6f7c827caf37b86b13f96f142ac496f07f358
JosephCon1998 <josephcon1998@gmail.com>
2018-12-26 15:04:25 -0500

    bar screen changes; navigationOptions now inside index.js for each screen of a navigation type

--- screens/BarStackNav/index.js
+++ screens/BarStackNav/index.js
@@ -8,8 +8,11 @@ import DiscussScreen from './Discuss.js';
 import EventsScreen from './Events.js';
 import MeetScreen from './Meet.js';
 import MenuScreen from './Menu.js';
-
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native'
 import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
+import { DrawerActions, withNavigation } from 'react-navigation';
 
 const StackContainer = createStackNavigator({
     Home: HomeScreen,
@@ -18,10 +21,25 @@ const StackContainer = createStackNavigator({
     Events: EventsScreen,
     Meet: MeetScreen,
     Menu: MenuScreen,
-},{ initialRouteName: 'Home'});
+},
+{ 
+    initialRouteName: 'Home', 
+    headerMode: 'float',
+    navigationOptions: ({ navigation }) => ({
+        headerTitle: `Manny's Bar`,
+        headerStyle: {
+          backgroundColor: 'rgba(16, 13, 100, 1)',
+        },
+        headerTitleStyle: {
+          fontFamily: 'HkGrotesk_Bold',
+          color: '#FFFFFF',
+        },
+        headerTintColor: '#fff',
+    })
+});
 
 const BarDrawerContainer = createDrawerNavigator({   // App
     screen: StackContainer,
-});
+},{ });
 
-export default BarDrawerContainer;
+export default withNavigation(BarDrawerContainer);
