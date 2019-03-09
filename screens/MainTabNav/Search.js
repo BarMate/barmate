3887543f876a91464c35e0ee2c807856b1d30067
JosephCon1998 <josephcon1998@gmail.com>
2019-03-09 13:12:31 -0500

    search changes

--- screens/MainTabNav/Search.js
+++ screens/MainTabNav/Search.js
@@ -1,419 +1,130 @@
-import React, { Component } from "react";
-import {
-  View,
-  StyleSheet,
-  Modal,
-  TouchableOpacity,
-  SafeAreaView,
-  TouchableWithoutFeedback,
-  FlatList
-} from "react-native";
-import { ListItem } from 'react-native-elements';
-import MapView, { Marker, Circle, AnimatedRegion } from "react-native-maps";
-import Variables from "../../config/Variables.js";
-import { Constants, Location, Permissions } from "expo";
-import { Ionicons } from "@expo/vector-icons";
-import MapBar from "../../components/BarComponent/MapBar";
-import {
-  Container,
-  Header,
-  Title,
-  Content,
-  Left,
-  Right,
-  Body,
-  Icon,
-  StyleProvider,
-  Text,
-  Button,
-  Item,
-  Input
-} from "native-base";
-import getTheme from "../../native-base-theme/components/index.js";
-import Common from "../../native-base-theme/variables/commonColor.js";
-import firebase from "../../config/Firebase.js";
-import SearchBar from "react-native-search-bar";
+/* 
+    Search.js
+    
+    The Search screen in BarMate
+    
+    Author:  Joseph Contumelio
+    Copyright(C) 2019, BarMate l.l.c.
+    All rights reserved
+*/
 
-console.disableYellowBox = true;
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import MapView from 'react-native-maps';
+import _ from 'lodash';
+
+const API_KEY = 'AIzaSyCN-KItGpvTPEhIMd9oG2CS8XldyOuVMAc';
 
 export default class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      region: {
-        latitude: 36.778259,
-        longitude: -119.417931,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      },
-      circleLatLng: {
-        latitude: 36.778259,
-        longitude: -119.417931
-      },
-      selectedMarker: {
-        name: "initial",
-        open: null,
-        rating: "initial",
-        price: 1,
-        description: "initial",
-        key: "initial"
-      },
-
-      locationResult: null,
-      barMarkers: [],
-      clubMarkers: [],
-      modalVisible: false,
-      search: "",
-      searchData: [],
-      searchActive: false
+      error: "",
+      longitude: 0,
+      latitude: 0,
+      destination: '',
+      predictions: [],
+      search: [],
     };
+    this.onChangeInputFieldDebounced = _.debounce(this.onChangeInputField, 1000);
   }
 
-  componentWillMount() {
-    this._getLocationAsync();
-  }
-
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status === "granted") {
-      console.log("Permission granted for LOCATION");
-      navigator.geolocation.getCurrentPosition(position => {
-        this.setState(prevState => ({
-          region: {
-            ...prevState.region,
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          },
-          circleLatLng: {
-            ...prevState.circleLatLng,
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-          }
-        }));
-        this._fetchMarkerData();
-      });
-    } else {
-      this.setState({
-        locationResult: "Permission denied for location access"
-      });
-    }
-  };
-
-  _fetchMarkerData() {
-    API_KEY = "AIzaSyCskpsQ0KdNBrxUaxDcp57PndW6xMRHOWY";
-
-    //=============================================================
-    // Creates a URL from users current position for local bars
-    // and local clubs
-    //=============================================================
+  componentDidMount() {
     navigator.geolocation.getCurrentPosition(
       position => {
-        const barUrl =
-          "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-          position.coords.latitude +
-          "," +
-          position.coords.longitude +
-          "&radius=5000&types=bar&key=" +
-          API_KEY;
-        const clubUrl =
-          "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" +
-          position.coords.latitude +
-          "," +
-          position.coords.longitude +
-          "&radius=5000&types=night_club&key=" +
-          API_KEY;
-        //=============================================================
-
-        //=============================================================
-        // Create an array to hold markers to be rendered
-        //=============================================================
-        var markersC = [];
-        var markersB = [];
-        //=============================================================
-
-        //=============================================================
-        // Find clubs at current location and push onto array
-        //=============================================================
-        fetch(clubUrl)
-          .then(data => data.json())
-          .then(res => {
-            for (var i = 0; i < res.results.length; i++) {
-              var instance = res.results[i];
-              //may have to change description
-              markersC.push({
-                name: instance.name,
-                coordinate: {
-                  latitude: instance.geometry.location.lat,
-                  longitude: instance.geometry.location.lng
-                },
-                description: "club " + i,
-                price: "price_level" in instance ? instance.price_level : 0,
-                open:
-                  "opening_hours" in instance
-                    ? instance.opening_hours.open_now
-                      ? "Open"
-                      : "Closed"
-                    : "N/A",
-                rating: "rating" in instance ? instance.rating : 0,
-                key: "place_id" in instance ? instance.place_id : null,
-                photos: instance.photos
-              });
-            }
-            this.setState({
-              clubMarkers: markersC
-            })
-              //causes warning to debugger but prevents android timer error
-              .catch(error => {
-                console.error(error);
-              });
-          });
-        //=============================================================
-
-        //=============================================================
-        // Find bars at current location and push onto array
-        //=============================================================
-        fetch(barUrl)
-          .then(data => data.json())
-          .then(res => {
-            for (var i = 0; i < res.results.length; i++) {
-              var instance = res.results[i];
-              //will not add a bar marker if it's already added as a club
-              if (markersB.indexOf(instance.place_id) <= -1) {
-                markersB.push({
-                  name: instance.name,
-                  coordinate: {
-                    latitude: instance.geometry.location.lat,
-                    longitude: instance.geometry.location.lng
-                  },
-                  description: "bar " + i,
-                  price: "price_level" in instance ? instance.price_level : 0,
-                  open:
-                    "opening_hours" in instance
-                      ? instance.opening_hours.open_now
-                        ? "Open"
-                        : "Closed"
-                      : "N/A",
-                  rating: "rating" in instance ? instance.rating : 0,
-                  key: "place_id" in instance ? instance.place_id : null,
-                  photos: "photos" in instance ? instance.photos : null
-                });
-              }
-            }
-            this.setState({
-              ...this.state,
-              barMarkers: markersB
-            })
-              //causes warning to debugger but prevents android timer error
-              .catch(error => {
-                console.error(error);
-              });
-          });
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
       },
-      error => this.setState({ ...this.state, error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 } 
+      error => this.setState({error: error.message}),
+      { enableHighAccuracy: true, maximumAge: 2000, timeout: 200000 }
     );
   }
 
-
-  _handleMapRegionChange = region => {
-    console.log("new region: " + JSON.stringify(region))
-    this.setState({ region });
-    this.fetchMarkerData().then(() => {this.forceUpdate();})
-  };
-
-  fetchSearchData(input) {
-    API_KEY = "AIzaSyCskpsQ0KdNBrxUaxDcp57PndW6xMRHOWY";
-    if(input === ""){
-      this.setState({searchActive: false});
+  async onChangeInputField(destination) {
+    this.setState({ destination });
+    const apiURL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${API_KEY}&input=${this.state.destination}&location=${this.state.latitude},${this.state.longitude}&radius=2000&types=establishment`;
+    try {
+      const result = await fetch(apiURL);
+      const json = await result.json();
+      this.setState({
+        predictions: json.predictions
+      })
+    } catch (err) {
+      console.log(err);
     }
-    else{
-      this.setState({searchActive: true});
+  }
+
+  async onSearch(place) {
+    const apiURL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${place}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${API_KEY}`
+    try {
+      const result = await fetch(apiURL);
+      const json = await result.json();
+      console.log(json);
+      this.setState({
+        search: json.candidates,
+        latitude: json.candidates[0].geometry.location.lat,
+        longitude: json.candidates[0].geometry.location.lng,
+      })
+    } catch(err) {
+      console.log(err);
     }
-
-    //console.log("Input string: ", input)
-
-    //===============================================
-    // Create url that will retrieve data for
-    // bars that are searched
-    //===============================================
-
-    const searchUrl = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
-    + input + "&type=bar" 
-    + "&key=" + API_KEY;
-
-    var searchResultsArr = [];
-
-    //console.log("SearchUrl: ", searchUrl);
-    
-    //Retrieve the data from the url search and put into array
-    fetch(searchUrl)
-    .then(data => data.json())
-    .then(res => {
-      this.setState({searchData: res.results});
-    });
   }
 
   render() {
-    if(this.state.searchActive === true){
-      return (
-        <StyleProvider style={getTheme(Common)}>
-          <Container>
-            <Header searchBar rounded>
-              <Item>
-                <Icon name="ios-wine" style={{color: '#FFFFFF'}}/>
-                <Input placeholder="Search" onChangeText={(text) => this.setState({search: text})/*this.fetchSearchData(text)*/}/>
-                <TouchableOpacity onPress={() => this.fetchSearchData(this.state.search)} >
-                  <Icon name="ios-search" style={{color: '#FFFFFF'}}/>
-                </TouchableOpacity>
-              </Item>
-            </Header>
-            <View style={styles.container}>
-              <FlatList
-                data={this.state.searchData}
-                keyExtractor={item => item.name}
-                renderItem={({ item }) => (
-                  <ListItem
-                    title={item.name}
-                    subtitle={item.formatted_address}/>
-                )}>
-              </FlatList>
-            </View>
-          </Container>
-        </StyleProvider>
-      );
-    }
-    else
-{
+
+    const predictions = this.state.predictions.map(prediction => 
+      <TouchableOpacity key={prediction.id} onPress={() => this.onSearch(prediction.description)}>
+        <Text style={styles.suggestions} key={prediction.id}>{prediction.description}</Text>
+      </TouchableOpacity>
+    )
+    
     return (
-      <StyleProvider style={getTheme(Common)}>
-        <Container>
-          <Header searchBar rounded>
-            <Item>
-              <Icon name="ios-wine" style={{color: '#FFFFFF'}}/>
-              <Input placeholder="Search" onChangeText={(text) => this.setState({search: text})/*this.fetchSearchData(text)*/}/>
-              <TouchableOpacity onPress={() => this.fetchSearchData(this.state.search)} >
-                <Icon name="ios-search" style={{color: '#FFFFFF'}}/>
-              </TouchableOpacity>
-            </Item>
-          </Header>
-          <Content>
-            <Modal
-              animationType="fade"
-              transparent={true}
-              visible={this.state.modalVisible}
-              backdropOpacity={0.5}>
-                <TouchableWithoutFeedback
-                  style={{width: Variables.deviceWidth, height: Variables.deviceHeight}}
-                  onPress={() => {
-                    this.setState({ modalVisible: false });}}>
-                  <View style={styles.modal}>
-                    <MapBar
-                      isMapComponent={true}
-                      barID={this.state.selectedMarker}/>
-                  </View>
-                </TouchableWithoutFeedback>
-            </Modal>
-            <View style={styles.container}>
-              <MapView
-                style={styles.map}
-                region={this.state.region}
-                showsUserLocation={true}
->           
-                <MapView.Circle
-                  center={this.state.circleLatLng}
-                  radius={5000}
-                  fillColor={"rgba(66, 19, 123, 0.1)"}
-                  strokeColor={"rgba(48, 44, 158, 0.3)"}/>
-
-                {this.state.barMarkers.map(markers => (
-                  <Marker
-                    key={markers.key}
-                    coordinate={{
-                      latitude: markers.coordinate.latitude,
-                      longitude: markers.coordinate.longitude
-                    }}
-                    onPress={() => {
-                      this.setState({
-                        ...this.state.selectedMarker,
-                        selectedMarker: {
-                          name: markers.name,
-                          open: markers.open,
-                          rating: markers.rating,
-                          price: markers.price,
-                          description: markers.description,
-                          key: markers.key,
-                          photos: markers.photos,
-                        },
-                        modalVisible: true
-                      });
-                    }}>
-                    <View>
-                      <Icon
-                        name="md-pin"
-                        style={{ color: "#030e2c", fontSize: 40 }}/>
-                    </View>
-                  </Marker>
-                ))}
-
-                {this.state.clubMarkers.map(markers => (
-                  <Marker
-                    key={markers.key}
-                    coordinate={{
-                      latitude: markers.coordinate.latitude,
-                      longitude: markers.coordinate.longitude
-                    }}
-                    title={markers.name}
-                    description={markers.description}
-                    onPress={() => {
-                      this.setState({
-                        ...this.state.selectedMarker,
-                        selectedMarker: {
-                          name: markers.name,
-                          open: markers.open,
-                          rating: markers.rating,
-                          price: markers.price,
-                          description: markers.description,
-                          key: markers.key,
-                          photos: markers.photos,
-                        },
-                        modalVisible: true
-                      });
-                      console.log(`PhotoURL: ${markers.photos}`)
-                    }}>
-                    <View>
-                      <Icon
-                        name="md-pin"
-                        style={{ color: "#1c2e63", fontSize: 40 }}/>
-                    </View>
-                  </Marker>
-                ))}
-              </MapView>
-            </View>
-          </Content>
-        </Container>
-      </StyleProvider>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          }}
+        />
+          <TextInput  
+            style={styles.destinationInput}
+            placeholder="Enter destination.." 
+            value={this.state.destination} 
+            onChangeText={destination => this.onChangeInputFieldDebounced(destination)}
+          />
+        {predictions}
+      </View>
     );
-}
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: Variables.deviceWidth,
-    height: Variables.deviceHeight
+    ...StyleSheet.absoluteFillObject
   },
   map: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    left: 0,
-    bottom: 0
+    ...StyleSheet.absoluteFillObject
   },
-  modal: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
-});
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: 'white',
+  },
+  suggestions: {
+    backgroundColor: 'white',
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+})
