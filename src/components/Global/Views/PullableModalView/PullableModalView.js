1b6753f4020413174679c1b903e8cf68e19c9867
JosephCon1998 <josephcon1998@gmail.com>
2019-06-19 15:42:13 -0400

    added react-native-modal and rn-responsive-screen; finished pullable modal; updated styles

--- src/components/Global/Views/PullableModalView/PullableModalView.js
+++ src/components/Global/Views/PullableModalView/PullableModalView.js
@@ -1,35 +1,92 @@
+/* 
+    PullableModalView.js
+    
+    Modal that can be pulled down using gestures
+    
+    Author:  Joseph Contumelio
+    Copyright(C) 2019, BarMate l.l.c.
+    All rights reserved
+*/
+
 import React, { Component } from "react";
 import {
   View,
   SafeAreaView,
-  Modal
+  ScrollView
 } from "react-native";
 
 import styles from './styles';
+import Modal from 'react-native-modal';
+
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+/**
+ * 
+ * props: visible: bool - should modal be visible
+ *        isScrollable: bool - should be true if modal has scrollable content
+ */
 
 class PullableModalView extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      visibleModal: this.props.visible,
+      scrollOffset: 0,
+    };
+  }
+
+
+  handleOnScroll = event => {
+    this.setState({
+      scrollOffset: event.nativeEvent.contentOffset.y,
+    });
+  };
+
+  handleScrollTo = p => {
+    if (this.scrollViewRef) {
+      this.scrollViewRef.scrollTo(p);
+    }
+  };
+
+  _determineRenderType() {
+    if(this.props.isScrollable == true) {
+      return (
+        <ScrollView
+          ref={ref => (this.scrollViewRef = ref)}
+          onScroll={this.handleOnScroll}
+          scrollEventThrottle={16}
+        >
+          {this.props.children}
+        </ScrollView>
+      )
+    }
+    else {
+      return (this.props.children)
+    }
   }
 
   render() {
     return (
       <Modal
-        animationType="slide"
-        transparent={true}
-        visible={this.props.visible}
-        onRequestClose={this.props.onRequestClose}
+        isVisible={this.state.visibleModal}
+        onSwipeComplete={() => this.setState({ visibleModal: false })}
+        onBackdropPress={() => this.setState({ visibleModal: false })}
+        swipeDirection={['down']}
+        style={{margin: 0}}
+        propagateSwipe
+        scrollTo={this.handleScrollTo}
+        scrollOffset={this.state.scrollOffset}
+        scrollOffsetMax={400 - 300} // content height - ScrollView height
       >
         <View style={styles.root}>
           {/* Pulldown bar Container */}
-          <SafeAreaView style={styles.pulldownBarContainer}>
+          <SafeAreaView onTouchEnd={() => this.setState({visibleModal: false})} style={styles.pulldownBarContainer}>
             <View style={styles.pulldownBar} />
           </SafeAreaView>
 
           {/* Content Container */}
           <View style={[styles.contentContainer, this.props.style]}>
-            {this.props.children}
+            {this._determineRenderType()}
           </View>
         </View>
       </Modal>
